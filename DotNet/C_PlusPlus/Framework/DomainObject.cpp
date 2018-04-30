4eb136820e82e169ab36c7b542b8b347c464f435
Enrico Velasco <enrico_velasco@hotmail.com>
2018-04-30 12:10:25 +0800

    Provide pImpl

--- DotNet/C_PlusPlus/Framework/DomainObject.cpp
+++ DotNet/C_PlusPlus/Framework/DomainObject.cpp
@@ -1,16 +1,45 @@
 #include <string>
-
 #include "stdafx.h"
+#include "Types.h"
 #include "DomainObject.h"
 
 using namespace std;
 using namespace Framework::Domain;
 
-string DomainObject::GetTestMessage()
-{
-	 return "Hello World"; 
-}
+class BaseQueryObject;
 
-DomainObject::~DomainObject()
+class DomainObject::Implementation
 {
+public:
+	enum InstantiationType { New = 1, Loaded = 2 };
+	typedef const unique_ptr<Guid> ConstGuidPtr;
+	typedef const BaseQueryObject ConstQueryObject;
+private:
+	ConstGuidPtr m_systemId;			//Data non-modifiable
+	ConstMapper *m_mapper;				//Data non-modifiable, pointer non-repointable
+	ConstQueryObject *m_queryObject;	//Data non-modifiable, pointer non-repointable
+public:
+	Implementation(ConstMapper *mapper) :
+		m_systemId(GenerateGuid()),
+		m_mapper(mapper),
+		m_queryObject(nullptr)
+	{
+	}
+	~Implementation() {}
+
+	//param type DomainObject is for back-reference
+	string GetTestMessage(const DomainObject& domainObject) const
+	{
+		return "Hello World";
+	}
+};
+
+DomainObject::DomainObject(ConstMapper *mapper) :
+	pImpl{ make_unique<Implementation>(mapper) } { }
+
+DomainObject::~DomainObject() { }
+
+string DomainObject::GetTestMessage() const
+{
+	return pImpl->GetTestMessage(*this);
 }
--- DotNet/C_PlusPlus/Framework/DomainObject.h
+++ DotNet/C_PlusPlus/Framework/DomainObject.h
@@ -2,13 +2,11 @@
 
 #include <string>
 #include <memory>
-#include "Types.h"
 #include "DllMacros.h"
 
 using namespace std;
 
 class BaseMapper;
-class BaseQueryObject;
 
 namespace Framework
 {
@@ -17,27 +15,16 @@ namespace Framework
 		class FRAMEWORK_API DomainObject
 		{
 		public:
-			enum InstantiationType { New = 1, Loaded = 2 };
-			typedef const unique_ptr<Guid> ConstGuidPtr;
+			static const DomainObject null;
 			typedef const BaseMapper ConstMapper;
-			typedef const BaseQueryObject ConstQueryObject;
-			
 		private:
-			ConstGuidPtr m_systemId;		//Data non-modifiable
-			ConstMapper *m_mapper;				//Data non-modifiable, pointer non-repointable
-			ConstQueryObject *m_queryObject;	//Data non-modifiable, pointer non-repointable
-
-		protected:
-			DomainObject(ConstMapper *mapper) :
-				m_systemId(GenerateGuid()),
-				m_mapper(mapper),
-				m_queryObject(nullptr)
-			{
-			}
-
+			class Implementation;
+			unique_ptr<Implementation> pImpl;
+		protected:								//Declaring DomainObject uninstantiable
+			DomainObject(ConstMapper*);
 		public:
-			virtual ~DomainObject() = 0;		//Declaring this DomainObject as abstract class
-			string GetTestMessage();
+			virtual ~DomainObject() = 0;		//Declaring DomainObject as abstract class
+			string GetTestMessage() const;
 		};
 	}
 }

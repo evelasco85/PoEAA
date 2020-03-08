1dfa01115c3231bc8d1f7e3c58d57710cf2cda83
Enrico <Enrico_Velasco@Hotmail.com>
2020-03-08 23:54:27 +0800

    Provided additional tests

--- NodeJs/Base.Tests/QueryObjectTests.js
+++ NodeJs/Base.Tests/QueryObjectTests.js
@@ -1,19 +1,89 @@
 'use strict';
 
 const assert = require('assert');
+const BaseQueryObject = require('./../Base/BaseQueryObject');
 const GetCustomerByIdQuery = require('./QueryObjects/GetCustomerByIdQuery');
-const query = new GetCustomerByIdQuery();
 
-query.searchInput = GetCustomerByIdQuery.createCriteria(2);
-
-query.execute((err, result) =>{
-    console.log("hello world");
-});
 
 describe('GetCustomerByIdQuery', function() {
+  describe('#searchInputTypeName', function() {
+    it("Get constructed 'searchInputTypeName' value", function(done) {
+      const query = new GetCustomerByIdQuery();
+      
+      query.searchInput = GetCustomerByIdQuery.createCriteria(2);
+      
+      assert.equal(query.searchInputTypeName, "GetCustomerByIdQuery.Criteria");
+      done();
+    });
+  });
+  describe('#execute()', function() {
+      it('Execute through promises', function(done) {
+        const query = new GetCustomerByIdQuery();
+        
+        query.searchInput = GetCustomerByIdQuery.createCriteria(2);
+        
+        query
+          .execute()
+          .then(result => {
+            assert.equal(result.length, 1);
+            assert.equal(result[0].id, 2);
+            assert.equal(result[0].name, "Jane Doe");
+  
+            done();
+          })
+          .catch(err => {
+            done(err);
+          });
+      });
+    });  
+  describe('#execute()', function() {
+    it('Execute through callback', function(done) {
+      const query = new GetCustomerByIdQuery();
+      
+      query.searchInput = GetCustomerByIdQuery.createCriteria(2);
+      
+      assert.equal(query.searchInputTypeName, "GetCustomerByIdQuery.Criteria");
+      done();
+    });
+  });
+});  
+  describe('BaseQueryObject', function() {
     describe('#execute()', function() {
-      it('should return -1 when the value is not present', function() {
-        assert.equal([1, 2, 3].indexOf(4), -1);
+      it('Execute through callback(w/error)', function(done) {
+        const query = new BaseQueryObject(null);
+        
+        query.searchInput = null;
+        
+        assert.equal(query.searchInputTypeName, "");
+        query.execute((err, result) =>{
+          // Single equals check for both `null` and `undefined`
+          if(err != null)
+          {
+            assert.equal(err.message, "'_performSearchOperation' overriding is required");
+          }
+
+          done();
+        });
+      });
+    });
+    describe('#execute()', function() {
+      it('Execute through promises(w/error)', function(done) {
+        const query = new BaseQueryObject(null);
+        
+        query.searchInput = null;
+        
+        assert.equal(query.searchInputTypeName, "");
+        query
+          .execute()
+          .catch(err => {
+            // Single equals check for both `null` and `undefined`
+            if(err != null)
+            {
+              assert.equal(err.message, "'_performSearchOperation' overriding is required");
+            }
+
+            done();
+          });
       });
     });
   });
--- NodeJs/Base/BaseQueryObject.js
+++ NodeJs/Base/BaseQueryObject.js
@@ -2,13 +2,18 @@
 
 class BaseQueryObject{
     constructor(searchInputType){
-        const subclassName = this.constructor.name;
-        const searchInputTypeName = searchInputType.name;
+        if((this.constructor.name) && (searchInputType) && (searchInputType.name))
+        {
+            const subclassName = this.constructor.name;
+            const searchInputTypeName = searchInputType.name;
 
-        this._searchInputTypeName = `${subclassName}.${searchInputTypeName}`;
+            this._searchInputTypeName = `${subclassName}.${searchInputTypeName}`;
+        }
+        else
+            this._searchInputTypeName = '';
     }
 
-    searchInputTypeName(){ return this._searchInputTypeName; }
+    get searchInputTypeName(){ return this._searchInputTypeName; }
 
     get searchInput(){ return this._searchInput; }
 
@@ -26,21 +31,32 @@ class BaseQueryObject{
         return new Promise((resolve, reject) =>{
             process.nextTick(() => {               
                 if(typeof this._performSearchOperation === 'function'){
-                    this._performSearchOperation(this._searchInput, (error, result) =>{
+                    try{
+                        this._performSearchOperation(this._searchInput, (error, result) =>{
+                            if(error){
+                                if(cb){
+                                    cb(error);
+                                }
+    
+                                return reject(error);
+                            }
+                            
+                            if(cb){
+                                cb(null, result);
+                            }
+    
+                            resolve(result);
+                        });
+                    }
+                    catch(error){
                         if(error){
                             if(cb){
                                 cb(error);
                             }
-
-                            return reject(error);
-                        }
-                        
-                        if(cb){
-                            cb(null, result);
                         }
 
-                        resolve(result);
-                    });
+                        return reject(error);
+                    }
                 }  
             });
         });

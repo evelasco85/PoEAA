8d7628f23a7dd844d5c7f79a0c8739bbe915a4aa
Enrico <Enrico_Velasco@Hotmail.com>
2020-04-13 21:24:10 +0800

    Initial mapper base implementation

--- NodeJs/Base/BaseMapper.js
+++ NodeJs/Base/BaseMapper.js
@@ -10,20 +10,28 @@ class BaseMapper{
     }
 
     /*Abstract methods*/    
-    _concreteUpdate(searchInput, resultCallback){
+    _concreteUpdate(entity, resultCallback){
         throw new Error("'_concreteUpdate' overriding is required"); 
     }
 
-    _concreteInsert(searchInput, resultCallback){
+    _concreteInsert(entity, resultCallback){
         throw new Error("'_concreteInsert' overriding is required"); 
     }
 
-    _concreteDelete(searchInput, resultCallback){
+    _concreteDelete(entity, resultCallback){
         throw new Error("'_concreteDelete' overriding is required"); 
     }
     /*****************/
 
-    update(entity, resultCallback)
+    update(entity, resultCallback){
+        _execute(_concreteUpdate, entity, resultCallback);
+    }
+
+    insert(entity, resultCallback){
+        _execute(_concreteInsert, entity, resultCallback);
+    }
+
+    _execute(operation, entity, resultCallback)
     {
         //Exposing callbacks and promises for public facing API's
         return new Promise((resolve, reject) =>{
@@ -36,9 +44,9 @@ class BaseMapper{
                     reject(error);
                 };
 
-                if(typeof this._concreteUpdate === 'function'){
+                if(typeof operation === 'function'){
                     try{
-                        this._concreteUpdate(this._searchInput, (error, result) =>{
+                        operation(entity, (error, result) =>{
                             // Single equals check for both `null` and `undefined`
                             if(error != null)
                                 return fnError(error, resultCallback);
@@ -56,6 +64,10 @@ class BaseMapper{
             });
         });
     }
+    
+    delete(entity, resultCallback){
+        _execute(_concreteDelete, entity, resultCallback);
+    }    
 }
 
 module.exports = BaseMapper;
